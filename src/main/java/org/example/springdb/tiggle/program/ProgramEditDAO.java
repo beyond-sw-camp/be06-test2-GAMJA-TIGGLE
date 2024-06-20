8c4dc1cbcb78d3f0304bd5a066598e3d985c0cca
yeon <dutls5367@gmail.com>
2024-06-20 17:31:05 +0900

    [삭제] 이미 정해진 틀이 있었다

--- src/main/java/org/example/springdb/tiggle/program/ProgramEditDAO.java
+++
@@ -1,5 +0,0 @@
-//            pstmt = connection.prepareStatement("UPDATE program SET ? = ? WHERE programId = ?");
-//            pstmt.setString(1, editDto.getColName());
-//            pstmt.setString(2, editDto.getColData());
-//            pstmt.setString(3, editDto.getProgramId().toString());
-
