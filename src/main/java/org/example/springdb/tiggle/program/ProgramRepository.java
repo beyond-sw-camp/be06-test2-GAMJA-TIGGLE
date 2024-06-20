4c3fa50ffad6ca7eb35993032e3e84f3f88c1d3f
yeon <dutls5367@gmail.com>
2024-06-20 17:27:38 +0900

    [Feat] 공연 목록 조회 구현

--- src/main/java/org/example/springdb/tiggle/program/ProgramRepository.java
+++ src/main/java/org/example/springdb/tiggle/program/ProgramRepository.java
@@ -2,8 +2,11 @@ package org.example.springdb.tiggle.program;
 
 
 import org.example.springdb.tiggle.program.model.ProgramCreateReq;
+import org.example.springdb.tiggle.program.model.ProgramReadRes;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ProgramRepository {
@@ -26,6 +29,19 @@ public class ProgramRepository {
                 dto.getSallerInfo(),
                 dto.getProgramStartDate(),
                 dto.getProgramEndDate());
+    }
+
+    public List<ProgramReadRes> finaAll(){
+        List<ProgramReadRes> programReadRes = jdbcTemplate.query(
+                "SELECT * FROM program",
+                (rs, rownum) -> {
+                    ProgramReadRes response = new ProgramReadRes(
+                            rs.getString("programName"),
+                            rs.getString("sallerInfo")
+                    );
+                    return response;
+                });
+        return programReadRes;
     }
 
 
